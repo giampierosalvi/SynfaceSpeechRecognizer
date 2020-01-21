469da678ca0605531e7ee3f699ecd4782d19af65
Giampiero Salvi <giampisalvi@gmail.com>
2020-01-21 17:57:28 +0100

    parsing configuration file

--- src/Configuration.c
+++ src/Configuration.c
@@ -21,13 +21,20 @@
  ***************************************************************************/
 
 #include "Configuration.h"
-#include "SoundIO.h"
-#include "SoundProc.h"
-#include "FeatureExtraction.h"
-#include "LikelihoodGen.h"
-#include "ViterbiDecoder.h"
+#include <libgen.h>
 
-/* there is a simpler function in LikelihoodGen.h, but this is the way it's done from Tcl */
+/* this is used to run a switch on strings */
+const unsigned long hash(const char *str) {
+    unsigned long hash = 5381;  
+    int c;
+
+    while ((c = *str++))
+        hash = ((hash << 5) + hash) + c;
+    return hash;
+}
+
+
+/* there is a simpler function in LikelihoodGen.h, but this is the way it's done from Tcl 
 int ReadANNAsInTCL(Recognizer *r, char *filename) {
   FILE *f;
   unsigned char *buffer;
@@ -37,7 +44,7 @@ int ReadANNAsInTCL(Recognizer *r, char *filename) {
   f = fopen(filename, "rb");
   if (!f) {
     fprintf(stderr, "cannot open file %s", filename);
-    error();
+    return -1;
   }
   buffer = (unsigned char *) malloc(MAX_FILE_SIZE*sizeof(unsigned char));
   n = fread(buffer, 1, MAX_FILE_SIZE, f);
@@ -49,45 +56,13 @@ int ReadANNAsInTCL(Recognizer *r, char *filename) {
   Recognizer_GetOutSym(r);
 
   return 0;
-}
+}*/
 
-int ReadANN(Recognizer *r, char *filename) {
+/* expecting three filenames separated by commas without spaces */
+int ReadGrammar(Recognizer *r, char *filenames, float gramfact, float insweight) {
   FILE *f;
-
-  f = fopen(filename, "rb");
-  if (!f) {
-    fprintf(stderr, "cannot open file %s", filename);
-    error();
-  }
-  LikelihoodGen_LoadANNFromFile(r->lg, f);
-  fclose(f);
-  Recognizer_GetOutSym(r);
-
-  return 0;
-}
-
-int ReadPhPrior(Recognizer *r, char *filename) {
-  FILE *f;
-  float tmp[200]; // priors are usually around of length 50
-  int n = 0;
-  Vector *pp;
-
-  f = fopen(filename, "r");
-  if (!f) {
-    fprintf(stderr, "cannot open file %s", filename);
-    error();
-  }
-  while(fscanf(f, "%f\n", &tmp[n]) != EOF) n++;
-  fclose(f);
-
-  pp = CreateVector(tmp,n);
-  LikelihoodGen_SetPhPrior(r->lg,pp);
-  return 0;
-}
-
-int ReadGrammar(Recognizer *r, char *filebasename, float gramfact, float insweight) {
-  FILE *f;
-  char filename[512];
+  int fn_idx;
+  char transmat_fn[MAX_PATH_LEN], prior_fn[MAX_PATH_LEN], map_fn[MAX_PATH_LEN], *fn_p[3], *src_p;
   int n = 0;
   int from[4000], to[4000], type[4000];
   float weight[4000];
@@ -95,150 +70,193 @@ int ReadGrammar(Recognizer *r, char *filebasename, float gramfact, float insweig
   Vector *stPrior;
   IntVector *fisStateId;
 
+  /* first copy path name */
+  fn_p[0] = transmat_fn;
+  fn_p[1] = prior_fn;
+  fn_p[2] = map_fn;
+  for(n=0; n<strlen(r->configDirName); n++) {
+    *fn_p[0]++ = r->configDirName[n];
+    *fn_p[1]++ = r->configDirName[n];
+    *fn_p[2]++ = r->configDirName[n];
+  }
+  *fn_p[0]++ = '\0';
+  *fn_p[1]++ = '\0';
+  *fn_p[2]++ = '\0';
+
+  /* now parse input parameter */
+  src_p = filenames;
+  fn_idx = 0;
+  while(*src_p) {
+      if(*src_p==',') {
+	*fn_p[fn_idx] = '\0';
+	src_p++;
+	fn_idx++;
+	if(fn_idx>2) return -1; /* too many commas */
+      }
+      *fn_p[fn_idx] = *src_p;
+      src_p++;
+      fn_p[fn_idx]++;
+  }
+  *fn_p[fn_idx] = '\0';
+  if(fn_idx!=2) return -1; /* too few commas */
+ 
   /* transition matrix */
-  sprintf(filename, "%s.transmat", filebasename);
-  f = fopen(filename, "r");
+  f = fopen(transmat_fn, "r");
   if (!f) {
-    fprintf(stderr, "cannot open file %s\n", filename);
-    error();
+    fprintf(stderr, "cannot open file %s\n", transmat_fn);
+    return -1;
   }
   while(fscanf(f, "%d %d %f %d\n", &from[n], &to[n], &weight[n], &type[n]) != EOF) {
     from[n]--; to[n]--; // convert from matlab to C indexes
     // here check Inf values (but there are none in the files I have checked)
     weight[n] *= -1;
+    /* This is now done by ViterbiDecoder_Activate
     if(type[n]) // apply grammar factor and insertion weight
       weight[n] = weight[n] * gramfact + insweight;
+    */
     n++;
   }
   fclose(f);
-  transmat = CreateSparseMatrix(from, to, weight, type, n);
+  transmat = SparseMatrix_CreateWithData(from, to, weight, type, n);
 
   /* state prior */
-  sprintf(filename, "%s.prior", filebasename);
   n=0;
-  f = fopen(filename, "r");
+  f = fopen(prior_fn, "r");
   if (!f) {
-    fprintf(stderr, "cannot open file %s\n", filename);
-    error();
+    fprintf(stderr, "cannot open file %s\n", prior_fn);
+    return -1;
   }
   while(fscanf(f, "%f\n", &weight[n]) != EOF) {
     from[n] *= -1; // this should work with the inf valuse as well
     n++;
   }
   fclose(f);
-  stPrior = CreateVector(weight,n);
+  stPrior = Vector_CrearteWithData(weight,n);
 
   /* fis state id */
-  sprintf(filename, "%s.map", filebasename);
   n=0;
-  f = fopen(filename, "r");
+  f = fopen(map_fn, "r");
   if (!f) {
-    fprintf(stderr, "cannot open file %s\n", filename);
-    error();
+    fprintf(stderr, "cannot open file %s\n", map_fn);
+    return -1;
   }
   while(fscanf(f, "%d\n", &from[n]) != EOF) {
     from[n]--; // convert from matlab to C indexes
     n++;
   }
   fclose(f);
-  fisStateId = CreateIntVector(from,n);
+  fisStateId = IntVector_CreateWithData(from,n);
 
   ViterbiDecoder_SetGrammar(r->vd,transmat,stPrior,fisStateId);
   return 0;
 }
 
-/* Warning! There is no checking on the values!!! */
-int Configuration_ApplyOption(char *option, char *value, Recognizer *r) {
-  switch(option) {
-    /* General parameters */
-  case "-ann":
-    ReadANN(r, value);
-    break;
-  case "-phprior":
-    ReadPhPrior(r, value);
-    break;
-  case "-decoder":
-    // fill this
-    break;
-    /* Feature Extraction parameters */
-  case "-fe:inputrate":
-    r->fe->inputrate = atoi(value);
-    break;
-  case "-fe:numfilters":
-    r->fe->numfilters = atoi(value);
-    break;
-  case "-fe:numceps":
-    r->fe->numceps = atoi(value);
-    break;
-  case "-fe:lowfreq":
-    r->fe->lowfreq = atof(value);
-    break;
-  case "-fe:hifreq":
-    r->fe->hifreq = atof(value);
-    break;
-  case "-fe:framestep":
-    r->fe->framestep = atof(value);
-    break;
-  case "-fe:framelen":
-    r->fe->framelen = atof(value);
-    break;
-  case "-fe:preemph":
-    r->fe->preemph = atof(value);
-    break;
-  case "-fe:lifter":
-    r->fe->lifter = atof(value);
-    break;
-    /* Viterbi decoder parameters */
-  case "-vt:grambasename":
-    ReadGrammar(r, value, 1, 0);
-    break;
-  case "-vt:lookahead":
-    Recognizer_SetLookahead(r, atoi(value));
-    break;
-  case "-vt:gramfact":
-    break;
-  case "-vt:insweight":
-    break;
-  default:
-    return -1
-      }
-}
-
+/* assuming MAX_PATH_LEN is allocated for dest */
 int joinPath(char *dest, const char *dirname, const char* filename) {
   int dirlen = strlen(dirname);
   int filelen = strlen(filename);
+  int destlen = dirlen+filelen+1;
+
+  if(destlen>=MAX_PATH_LEN)
+    return -1;
 
   strcpy(dest, dirname);
   dest[dirlen] = '/'; // this should be portable across platforms
-  for(i=0;i<filelen, i++)
+  for(int i=0; i<filelen; i++)
     dest[dirlen+1+i] = filename[i];
-  // check null symbol at the end
+  dest[destlen] = '\0';
   
   return dirlen+1+filelen;
 }
 
-int Configuration_ApplyConfig(char *dirname, Recognizer *r) {
-  char *tmpname, nameptr;
-  int dirlen = strlen(dirname);
-  int i;
-  
-  DBGPRINTF("applyng configuration in %s\n", dirname);
-  /* allocate enough memory for file names */
-  tmpname = (char *) malloc(dirlen+100);
+int Configuration_ApplyOption(Recognizer *r, char *option, char *value) {
+  char filename[MAX_PATH_LEN];
 
-  joinPath(tmpname, dirname, PARAMETERS_FN);
-
-  joinPath(tmpname, dirname, RNN_FN);
-
-  joinPath(tmpname, dirname, PHONE_PRIOR_FN);
-
-  joinPath(tmpname, dirname, HMM_MAP_FN);
-
-  joinPath(tmpname, dirname, HMM_PRIOR_FN);
-
-  joinPath(tmpname, dirname, HMM_TRANSMAT_FN);
-
-  free(tmpname);
+  switch(hash(option)) {
+    /* General parameters */
+      case CONF_LG_ANN: /* -lg:ann */
+    if(joinPath(filename, r->configDirName, value) != 0)
+      return -1;
+    LikelihoodGen_LoadANNFromFilename(r->lg, filename);
+    Recognizer_GetOutSym(r);
+    break;
+      case CONF_LG_PHPRIOR: /* -lg:phprior */
+    if(joinPath(filename, r->configDirName, value) != 0)
+      return -1;
+    Vector *pp = Vector_LoadFromFilename(filename);
+    LikelihoodGen_SetPhPrior(r->lg,pp);
+    break;
+      case CONF_DECODER: /* -decoder */
+    // fill this
+    break;
+    /* Feature Extraction parameters */
+      case CONF_FE_INPUTRATE: /* -fe:inputrate */
+    r->fe->inputrate = atoi(value);
+    break;
+      case CONF_FE_NUMFILTERS: /* -fe:numfilters */
+    r->fe->numfilters = atoi(value);
+    break;
+      case CONF_FE_NUMCEPS: /* -fe:numceps */
+    r->fe->numceps = atoi(value);
+    break;
+      case CONF_FE_LOWFREQ: /* -fe:lowfreq */
+    r->fe->lowfreq = atof(value);
+    break;
+      case CONF_FE_HIFREQ: /* -fe:hifreq */
+    r->fe->hifreq = atof(value);
+    break;
+      case CONF_FE_FRAMESTEP: /* -fe:framestep */
+    r->fe->framestep = atof(value);
+    break;
+      case CONF_FE_FRAMELEN: /* -fe:framelen */
+    r->fe->framelen = atof(value);
+    break;
+      case CONF_FE_PREEMPH: /* -fe:preemph */
+    r->fe->preemph = atof(value);
+    break;
+      case CONF_FE_LIFTER: /* -fe:lifter */
+    r->fe->lifter = atof(value);
+    break;
+    /* Viterbi decoder parameters */
+      case CONF_VT_HMM: /* -vt:hmm */
+    ReadGrammar(r, value, 1, 0);
+    break;
+      case CONF_VT_LOOKAHEAD: /* -vt:lookahead */
+    Recognizer_SetLookahead(r, atoi(value));
+    break;
+      case CONF_VT_GRAMFACT: /* -vt:gramfact */
+    r->vd->gramfact = atof(value);
+    break;
+      case CONF_VT_INSWEIGHT: /* -vt:insweight */
+    r->vd->insweight = atof(value);
+    break;
+  default:
+    return -1;
+      }
+  return 0;
 }
 
-#endif /* _CONFIGURATION_H_ */
+int Configuration_ApplyConfigFromFilename(Recognizer *r, char *filename) {
+  char tmpname[MAX_PATH_LEN];
+  FILE *fp = NULL;
+  char option[1024], value[1024];
+
+  strcpy(tmpname, filename); /* make a copy because dirname modifies it */
+  strcpy(r->configDirName, dirname(tmpname));
+  
+  DBGPRINTF("applyng configuration in %s\n", filename);
+
+  fp = fopen(filename, "r");
+  if (fp == NULL) {
+      fprintf(stderr, "could not open file: %s\n", filename);
+      return -1;
+  }
+  
+  while(fscanf(fp, "%s %s\n", option, value) == 2) {
+      Configuration_ApplyOption(r, option, value);
+  }
+
+  fclose(fp);
+  
+  return 0;
+}
--- src/Configuration.h
+++ src/Configuration.h
@@ -24,11 +24,6 @@
 #define _CONFIGURATION_H_
 
 #include "Recognizer.h"
-#include "SoundIO.h"
-#include "SoundProc.h"
-#include "FeatureExtraction.h"
-#include "LikelihoodGen.h"
-#include "ViterbiDecoder.h"
 
 /* standard model file names: each configuration directory is
    expected to contain the following files */
@@ -39,7 +34,27 @@
 #define HMM_PRIOR_FN    "hmm_prior.txt"
 #define HMM_TRANSMAT_FN "hmm_transmat.txt"
 
-int Configuration_ApplyConfig(char *dirname, Recognizer *r);
+/* definitions that allow to run a switch on strings using a hash function.
+ See generate_config_hash.c for more details */
+#define CONF_LG_ANN        229392308683868     /* -lg:ann */
+#define CONF_LG_PHPRIOR   -4660004979818057853 /* -lg:phprior */
+#define CONF_DECODER       7569935825667912    /* -decoder */
+#define CONF_FE_INPUTRATE -2197128137810322605 /* -fe:inputrate */
+#define CONF_FE_NUMFILTERS 1281989501715611648 /* -fe:numfilters */
+#define CONF_FE_NUMCEPS   -4660286263619983022 /* -fe:numceps */
+#define CONF_FE_LOWFREQ   -4660286266425750505 /* -fe:lowfreq */
+#define CONF_FE_HIFREQ     8243662873751427158 /* -fe:hifreq */
+#define CONF_FE_FRAMESTEP -2197132206265240770 /* -fe:framestep */
+#define CONF_FE_FRAMELEN  -6215494455062744735 /* -fe:framelen */
+#define CONF_FE_PREEMPH   -4660286261163860056 /* -fe:preemph */
+#define CONF_FE_LIFTER     8243662873907970909 /* -fe:lifter */
+#define CONF_VT_HMM        229392715462360     /* -vt:hmm */
+#define CONF_VT_LOOKAHEAD -1365476488673139138 /* -vt:lookahead */
+#define CONF_VT_GRAMFACT  -6190292976414017381 /* -vt:gramfact */
+#define CONF_VT_INSWEIGHT -1365480744877158200 /* -vt:insweight */
+
+
+int Configuration_ApplyConfigFromFilename(Recognizer *r, char *filename);
 
 
 #endif /* _CONFIGURATION_H_ */
